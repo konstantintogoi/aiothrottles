f5b8281fb04a8a4fdfe64aa7e6aa2376ad87cc83
Konstantin Togoi <konstantin.togoi@gmail.com>
2020-03-19 09:45:16 +0300

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='aiothrottles',
-    version='0.1.0',
+    version='0.1.1',
     author='Konstantin Togoi',
     author_email='konstantin.togoi@protonmail.com',
     url='https://github.com/KonstantinTogoi/aiothrottles',
